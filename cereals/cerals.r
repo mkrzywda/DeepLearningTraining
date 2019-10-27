d4f4ba6ea631bbaeb22b41b1a1bfbafaa65c90b9
Maciej Krzywda <maciej.krzywdaa@gmail.com>
2019-10-27 13:39:12 +0100

    Sampling a creating training data set

---
+++ cereals/cerals.r
@@ -0,0 +1,13 @@
+## Creating index variable 
+
+# Read the Data
+data = read.csv("cereals.csv", header=T)
+
+# Random sampling
+samplesize = 0.60 * nrow(data)
+set.seed(80)
+index = sample( seq_len ( nrow ( data ) ), size = samplesize )
+
+# Create training and test set
+datatrain = data[ index, ]
+datatest = data[ -index, ]
